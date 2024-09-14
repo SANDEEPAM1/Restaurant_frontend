935cbd748ddee2319544864e4448c648d77ca86d
SANDEEPA1 <sandeepamadhuranga169@gmail.com>
2024-09-14 21:36:57 +0530

    card Component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <>
+    <div>
+        <div className="imgae">
+            <img src="" alt="food photo" />
+        </div>
+        <div className="foodName">
+                <h1> </h1>
+        </div>
+        <div className='description'>
+            <p> </p>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Card
