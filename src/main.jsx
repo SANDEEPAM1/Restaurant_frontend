2411306f062cb93fda34522ea9e6a3e9e6caf216
SANDEEPA1 <sandeepamadhuranga169@gmail.com>
2024-11-04 05:06:36 +0530

    url of admin page

--- src/main.jsx
+++ src/main.jsx
@@ -1,84 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import Home from './pages/Home.jsx'
-import Menu from './pages/Menu.jsx'
-import Order from './pages/Order.jsx'
-import Reservation from './pages/Reservation.jsx'
-import Offers from './pages/Offers.jsx'
-import Galary from './pages/Galary.jsx'
-import KictchenDisplay from './pages/KictchenDisplay.jsx'
-import Admin from './pages/Admin.jsx'
-import ViewOrders from './components/Admin/ViewOrders.jsx'
 import {createBrowserRouter,RouterProvider } from 'react-router-dom'
-import routes from './Routes/UrlRoutes.jsx'
+import {router} from './Routes/UrlRoutes.jsx'
 import './index.css'
-import ViewDeliveryPerson from './components/Admin/ViewDeliveryPerson.jsx'
-import MenuItems from './components/Admin/MenuItems.jsx'
-import PhyaicalTables from './components/Admin/PhyaicalTables.jsx'
 
 
-
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element:<App/>,
-    children:[
-      {
-          path:'/',
-          element:<Home/>,  
-      },
-      {
-          path:'/menu',
-          element:<Menu/>
-      },
-      {
-          path:'/order',
-          element:<Order/>
-      },
-      {
-          path:'/reservation',
-          element:<Reservation/>
-      },
-      {
-          path:'/offers',
-          element:<Offers/>
-      },
-      {
-          path:'/gallary',
-          element:<Galary/>
-      }
-  ]
-  },
-  {
-    path:"/kitchen",
-    element:<KictchenDisplay/>
-  },
-  {
-    path:"/Admin",
-    element:<Admin/>,
-    children:[
-      {
-        path:"/Admin/viewOrders",
-        element:<ViewOrders/>
-      },
-      {
-        path:"/Admin/viewdeliveryperson",
-        element:<ViewDeliveryPerson/>
-      },
-      {
-        path:"/Admin/menuItems",
-        element:<MenuItems/>
-      },
-      {
-        path:"/Admin/physicalTables",
-        element:<PhyaicalTables/>
-      }
-
-    ]
-  }
-
-])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
 import { IoIosNotificationsOutline,IoMdMenu  } from "react-icons/io";
-import { IoPerson } from "react-icons/io5"
+import { IoPerson } from "react-icons/io5"  
+import { adminUrls } from '../Routes/UrlRoutes';
+import { NavLink } from 'react-router-dom';
+import '../components/NavBar/NavBar.css'
 
 const Admin = () => {
   return (
@@ -15,9 +18,9 @@ const Admin = () => {
 
         <div className="flex main-contain ">
             <div className="sidebar w-[15%] h-[93vh] flex flex-col bg-gray-300 overflow-hidden justify-center items-center  ">
-            <a href="/">Orders</a>
-            <a href="/">Add Users</a>
-            <a href="/">click</a>
+            {adminUrls.map((route, index)=>{
+                return <NavLink to={route.path} key={index} className={({isActive})=>(isActive?'active-link':'link')}>{route.title}</NavLink>
+            })}
             </div>
 
             <div className="mx-8 my-4 h-[88vh] content w-[80%] ">
